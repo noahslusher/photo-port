06c9806877a5f5171ec6b090642deb9c6e6bd2d7
Carl Wheezer <slushern42@gmail.com>
2022-06-01 14:57:36 -0600

    added hooks

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,33 +1,29 @@
 import React from 'react'
-const categories = [
- {
-   name: "Commercial",
-   description:
-     "Photos of grocery stores, food trucks, and other commercial projects",
- },
- { name: "Portraits", description: "Portraits of people in my life" },
- { name: "Food", description: "Delicious delicacies" },
- {
-   name: "Landscape",
-   description: "Fields, farmhouses, waterfalls, and the beauty of nature",
- },
-];
+import { capitalizeFirstLetter } from "../../utils/helpers";
+import React, { useEffect } from 'react'
 
-function Nav() {
- function categorySelected(name) {
-  console.log(`${name} clicked`)
- }
+function Nav(props) {
+ const {
+  categories = [],
+  setCurrentCategory,
+  currentCategory,
+} = props;
+
+useEffect(() => {
+ document.title = capitalizeFirstLetter(currentCategory.name);
+}, [currentCategory]);
+ 
  return (
-  <header>
+  <header className="flex-row px-1">
   <h2>
-    <a href="/">
-      <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+    <a data-testid="link" href="/">
+      <span role="img" aria-label="camera">📸</span>Oh Snap!
     </a>
   </h2>
   <nav>
     <ul className="flex-row">
       <li className="mx-2">
-        <a href="#about">
+        <a data-testid="about" href="#about">
           About me
         </a>
       </li>
@@ -36,11 +32,11 @@ function Nav() {
       </li>
        {categories.map((category) => (
         <li
-          className="mx-1"
+          className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`}
           key={category.name}
         >
-          <span onClick={() => categorySelected(category.name)}>
-            {category.name}
+          <span onClick={() => {setCurrentCategory(category)}}>
+           {capitalizeFirstLetter(category.name)}
           </span>
         </li>
       ))}
