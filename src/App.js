c889dbf82e2bac92ff50bbe63f1359ae419048a5
Carl Wheezer <slushern42@gmail.com>
2022-06-02 09:08:47 -0600

    single page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, {useState} from 'react'
-import './App.css';
-import About from './components/About'
-import Nav from './components/Nav'
-import Gallery from './components/Gallery'
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
-  const [categories] = useState([
+  const [categories] = useState([ 
     {
       name: 'commercial',
       description: 'Photos of grocery stores, food trucks, and other commercial projects',
@@ -18,17 +17,26 @@ function App() {
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div>
-      <Nav>
-      categories={categories}
+      <Nav
+        categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-      </Nav>
-      <main> 
-      <ContactForm></ContactForm>
-      <Gallery currentCategory={currentCategory}></Gallery>
-      <About></About>
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
       </main>
     </div>
   );
