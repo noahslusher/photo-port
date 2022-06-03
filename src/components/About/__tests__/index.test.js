033d0ca7bf87b398cda1002fc925a1519cd10d10
Carl Wheezer <slushern42@gmail.com>
2022-06-02 21:09:54 -0600

    finished and deployed

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,20 +1,19 @@
-import React from 'react'
+// __tests__/About.test.js
+import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import About from '..'
+import About from '..';
 
-afterEach(cleanup)
+afterEach(cleanup);
 
 describe('About component', () => {
- // First Test
- it('renders', () => {
-  render(<About />)
- })
+  it('renders', () => {
+    render(<About />);
+  });
 
- // Second Test
- it('matches snapshot DOM node structure', () => {
-  // render About
-  const { asFragment } = render(<About />)
-  expect(asFragment()).toMatchSnapshot()
-});
+  it('matches snapshot', () => {
+    const { asFragment } = render(<About />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
